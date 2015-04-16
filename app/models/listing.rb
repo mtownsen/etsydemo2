0161965be3288d202ee0ad1e1db77ccf15bfe894
Matthew Townsen <matthewtownsen@gmail.com>
2015-04-15 21:09:03 -0600

    install and configure devise

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,5 @@
 class Listing < ActiveRecord::Base
-  if rails.env.development?
+  if Rails.env.development?
   has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   has_attached_file :avatar,
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Required for Devise GEM
+  # Remember to change localhost:3000 to prod
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
